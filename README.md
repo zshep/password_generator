a3f985be8b532ea69d2860557a91fafb195fc9e0
zsheperdigian <zsheperdigian@gmail.com>
2022-07-25 13:47:31 -0700

    completed readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Password Generator Starter Code
+# Password Generator
+
+ The application will ask the user for different password criteria including length and character type. The application will then take that criteria and generate a random password where the user can copy and paste the new password. 
+
+![Alt text](./screenshot.jpg?raw=true "Screenshot")
+
+Link to application:file:///D:/programing/UW%20bootcamp/challenges/password_generator/index.html
--- script.js
+++ script.js
@@ -49,21 +49,25 @@ function gencrit(length_pw) {
   if (crit_abc == true) {
     genpw = genpw.concat(abc);
     console.log( `you want lowercase values`);
+    window.alert(`you want lowercase values`);
   }
   //if yes, then add the type to genpw
   if (crit_ABC == true) {
     genpw = genpw.concat(ABC);
     console.log( `you want uppercase values`);
+    window.alert(`you want uppercase values`);
   }
   //if yes, then add the type to genpw
   if (crit_num == true) {
     genpw = genpw.concat(num);
     console.log( `you want number values`);
+    window.alert(`you want number values`);
   }
   //if yes, then add the type to genpw
   if (crit_spec == true) {
     genpw = genpw.concat(spec);
     console.log( `you want special values`);
+    window.alert(`you want special values`);
   }
 
   //Use for loop to decide length
